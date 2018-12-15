17be768a00c13cfa16fa0dc2459bedda81d872c3
Robert Berryhill <robjberryhill@gmail.com>
2018-12-15 13:09:15 -0600

    FirstCommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+const styles = {
+  fontFamily: "sans-serif",
+  textAlign: "center"
+};
+let APIUrl = "https://jsonplaceholder.typicode.com/posts";
+class App extends React.Component {
+  state = {
+    exercises: [],
+    errors: null
+  };
+
+  componentDidMount() {
+    fetch(APIUrl)
+      .then(r => r.json())
+      .then(p => {
+        let first10 = p.filter((i, index) => index < 10);
+        console.log(first10);
+        this.setState({exercises: first10})
+      })
+      .catch(err => this.setState({ error: err }));
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div class="App">
+        {this.state.exercises.map(p => (
+          <p key={p.id}>{p.title}</p>
+        ))}
       </div>
     );
   }
